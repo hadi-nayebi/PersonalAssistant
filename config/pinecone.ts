136dd2d8b9148ca58f5c863b18eb07df52233238
hadinayebi <hadinayebi@gmail.com>
2023-05-08 15:20:15 -0700

    adding three types of memory to ingest

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -8,6 +8,9 @@ if (!process.env.PINECONE_INDEX_NAME) {
 
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
 
-const PINECONE_NAME_SPACE = 'personal-assistant'; //namespace is optional for your vectors
+// const PINECONE_NAME_SPACE = 'personal-assistant'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE_LM = 'personal-assistant-lm'; // namespace for long-term memory
+const PINECONE_NAME_SPACE_SM = 'personal-assistant-sm'; // namespace for short-term memory
+const PINECONE_NAME_SPACE_WM = 'personal-assistant-wm'; // namespace for working memory
 
-export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
+export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE_LM, PINECONE_NAME_SPACE_SM, PINECONE_NAME_SPACE_WM };
--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -3,16 +3,19 @@ import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { pinecone } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE_LM, PINECONE_NAME_SPACE_SM, PINECONE_NAME_SPACE_WM } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 
 /* Name of directory to retrieve your files from */
-const filePath = 'docs';
+const filePath_lm = 'docs_lm';
+const filePath_sm = 'docs_sm';
+const filePath_wm = 'docs_wm';
 
 export const run = async () => {
+  // Ingesting long term memory docs into Pinecone
   try {
     /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
+    const directoryLoader = new DirectoryLoader(filePath_lm, {
       '.pdf': (path) => new CustomPDFLoader(path),
     });
 
@@ -31,17 +34,87 @@ export const run = async () => {
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
     const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
 
     //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
+      namespace: PINECONE_NAME_SPACE_LM,
       textKey: 'text',
     });
   } catch (error) {
     console.log('error', error);
-    throw new Error('Failed to ingest your data');
+    throw new Error('Failed to ingest long term memory data');
+  }
+
+  // Ingesting short term memory docs into Pinecone
+  try {
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(filePath_sm, {
+      '.pdf': (path) => new CustomPDFLoader(path),
+    });
+
+    // const loader = new PDFLoader(filePath);
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log('split docs', docs);
+
+    console.log('creating vector store...');
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: PINECONE_NAME_SPACE_SM,
+      textKey: 'text',
+    });
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest short term memory data');
+  }
+
+  // Ingesting working memory docs into Pinecone
+  try {
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(filePath_wm, {
+      '.json': (path) => new CustomPDFLoader(path),
+    });
+
+    // const loader = new PDFLoader(filePath);
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log('split docs', docs);
+
+    console.log('creating vector store...');
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: PINECONE_NAME_SPACE_WM,
+      textKey: 'text',
+    });
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest working memory data');
   }
 };
 
